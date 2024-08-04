a636f4fb2ad42806dd0f1239ef305493bcf0657d
Malcom Calivar <arkmalcom@gmail.com>
2024-08-04 07:55:50 -0400

    added our story and lightbox

--- src/scripts/ourStory.ts
+++ src/scripts/ourStory.ts
@@ -1,17 +1,48 @@
+import "fslightbox";
+
 import watercolorBg from "../assets/watercolor-bg.jpg";
+import pic1 from "../assets/gallery/pic1.jpg";
+import pic2 from "../assets/gallery/pic2.jpg";
+import pic3 from "../assets/gallery/pic3.jpg";
+import pic4 from "../assets/gallery/pic4.jpg";
+import pic5 from "../assets/gallery/pic5.jpg";
 
 const ourStory = document.createElement("section");
 ourStory.className = "section relative w-screen h-screen bg-orange-50";
 ourStory.id = "our-story";
 ourStory.innerHTML = `
-  <div class="flex w-screen h-screen flex-col items-center justify-center border-b-4 border-t-4 border-amber-950">\
+  <div class="flex w-screen h-screen flex-col items-center justify-start border-b-4 border-amber-950">
     <img src="${watercolorBg}" alt="watercolor background" class="absolute object-cover object-bottom-left top-0 w-screen h-screen opacity-25" />
-    <div class="flex flex-col space-y-1 pt-4 w-screen justify-center items-center">
+    <div class="flex flex-col space-y-1 pt-4 w-screen items-center">
         <div class="border-t border-2 border-amber-900 lg:w-96 w-2/3"></div>
         <p i18n-key="ourStory.title" class="font-logo text-4xl p-2 text-amber-900 font-bold"></p>
         <div class="border-b border-2 border-amber-900 lg:w-96 w-2/3"></div>
     </div>
+    <div class="container mx-auto mt-10">
+      <div class="relative w-full overflow-hidden">
+        <div class="flex items-center justify-center space-x-4">
+          <a href="${pic2}" data-fslightbox="gallery" class="transform transition-transform duration-200 hover:scale-105">
+            <img src="${pic2}" alt="gallery image" class="lg:block hidden max-w-full border-amber-50 border-4 h-auto rounded-lg shadow-md" />
+          </a>
+          <a href="${pic1}" data-fslightbox="gallery" class="transform transition-transform duration-200 hover:scale-105">
+            <img src="${pic1}" alt="gallery image" class="max-w-full border-amber-50 border-4 h-auto rounded-lg shadow-md" />
+          </a>
+          <a href="${pic3}" data-fslightbox="gallery" class="transform transition-transform duration-200 hover:scale-105">
+            <img src="${pic3}" alt="gallery image" class="lg:block hidden max-w-full border-amber-50 border-4 h-auto rounded-lg shadow-md" />
+          </a>
+          <a href="${pic4}" data-fslightbox="gallery" class="transform transition-transform duration-200 hover:scale-105">
+          </a>
+          <a href="${pic5}" data-fslightbox="gallery" class="transform transition-transform duration-200 hover:scale-105">
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 `;
+
+document.body.appendChild(ourStory);
+setTimeout(() => {
+  (window as any).refreshFsLightbox();
+}, 0);
 
 export default ourStory;
--- src/scripts/party.ts
+++ src/scripts/party.ts
@@ -4,31 +4,30 @@ const party = document.createElement("section");
 party.className = "section relative w-screen h-screen";
 party.id = "party";
 party.innerHTML = `
-  <div class="flex w-screen h-screen flex-col items-center justify-start border-b-4 border-t-4 border-amber-950 space-y-2">
+  <div class="flex w-screen h-screen flex-col items-center justify-start border-b-4 border-amber-950 lg:space-y-8 space-y-2">
     <img src="${watercolorBg}" alt="watercolor background" class="absolute object-cover object-right top-0 w-screen h-screen opacity-25" />
     <div class="flex flex-col space-y-1 pt-4 w-screen justify-center items-center">
       <div class="border-t border-2 border-amber-900 lg:w-96 w-2/3"></div>
       <p i18n-key="party.title" class="font-logo text-4xl p-2 text-amber-900 font-bold"></p>
       <div class="border-b border-2 border-amber-900 lg:w-96 w-2/3"></div>
     </div>
-    <div class="flex flex-col space-y-6 pt-4 w-screen justify-center items-center">
+    <div class="flex flex-col lg:space-y-12 space-y-6 pt-4 w-screen justify-center items-center">
     <div class="flex flex-col rounded-lg shadow-md bg-amber-50 w-80 h-72 justify-start items-center z-10 p-2">
         <p i18n-key="party.musicHeader" class="text-amber-950 text-3xl p-2 font-semibold font-stylized"></p>
         <p i18n-key="party.musicContent" class="text-amber-950 text-sm text-base p-2 pb-10"></p>
         <a href="#" target="_blank" rel="noopener noreferrer">
-            <button i18n-key="party.recommendSong" class="fade-button opacity-0 py-1 h-20 w-64 rounded-md border-4 shadow-md px-2 border-amber-950 bg-amber-800 text-amber-100 uppercase font-black font-serif hover:text-amber-800 text-lg"></button>
+            <button i18n-key="party.recommendSong" class="fade-button opacity-0 py-1 h-20 w-64 rounded-md border-4 shadow-md px-2 border-amber-950 bg-amber-800 text-amber-100 uppercase font-black font-serif hover:text-amber-200 text-lg"></button>
         </a>
     </div>
     <div class="flex flex-col rounded-lg shadow-md bg-amber-50 w-80 h-72 justify-start items-center z-10 p-2">
         <p i18n-key="party.dressCodeHeader" class="text-amber-950 text-3xl p-2 font-semibold font-stylized"></p>
         <p i18n-key="party.dressCodeContent" class="text-amber-950 text-sm text-base p-2"></p>
         <a href="#" target="_blank" rel="noopener noreferrer">
-            <button i18n-key="party.dressCodeButton" class="relative top-2/3 fade-button opacity-0 py-1 h-20 w-64 rounded-md border-4 shadow-md px-2 border-amber-950 bg-amber-800 text-amber-100 uppercase font-black font-serif hover:text-amber-800 text-lg"></button>
+            <button i18n-key="party.dressCodeButton" class="relative top-2/3 fade-button opacity-0 py-1 h-20 w-64 rounded-md border-4 shadow-md px-2 border-amber-950 bg-amber-800 text-amber-100 uppercase font-black font-serif hover:text-amber-200 text-lg"></button>
         </a>
     </div>
     </div>
   </div>
 `;
-
 
 export default party;
